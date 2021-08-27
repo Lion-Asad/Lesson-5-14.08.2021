6bbe501a615465d14f05572e8d4b12ebc512cf5b
Hasanov Asadullo Abduvosievich <88389289+Lion-Asad@users.noreply.github.com>
2021-08-27 14:46:13 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,83 +1,101 @@
-﻿using System;
+using System;
 
-namespace Lesson5
+class Program
 {
-     class Program
-    {
-        static void Main(string[] args)
-        {
-            // Задание 2
 
-            Console.Write("Размерность массива: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[n];
-            for (int i = 0; i < array.Length; i++)
+    static void Main()
+    {
+        // Задание 1
+        Console.Write("Введите размер массива: ");
+        int ns = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите определение массива:");
+        int[] arr = new int[ns];
+        for (int i = 0; i < ns; ++i)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int max = arr[0], min = arr[0], sum = 0;
+        for (int i = 0; i < ns; ++i)
+        {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+            sum += arr[i];
+
+
+        }
+        double arif = 1.0 * sum / arr.Length;
+        Console.WriteLine($"Макс: {max}");
+        Console.WriteLine($"Мин: {min}");
+        Console.WriteLine($"Сумма: {sum}");
+        Console.WriteLine($"Середина: {arif}");
+        Console.Write("Единственное число: ");
+        
+        for (int i = 0; i < ns; ++i)
+            if (arr[i] % 2 != 0)
             {
-                Console.Write("array[{0}] = ", i);
-                array[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"{arr[i]} ");
             }
-            int min = 0;
-            for (int i = 1; i < array.Length; i++)
+System.Console.WriteLine("\n");
+        // задание 2
+        Console.Write("Введите размер массива: ");
+        int nn = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите определение массива:");
+        int[] m = new int[nn];
+        int[] rez = new int[nn];
+        for (int i = 0; i < nn; i++)
+        {
+
+            m[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < nn; ++i)
+        {
+            rez[i] = m[m.Length - i - 1];
+        }
+        for (int i = 0; i < nn; ++i)
+        {
+            Console.Write($"{rez[i]} ");
+        }
+System.Console.WriteLine("\n");
+        // Задание 3
+        Console.Write("Введите размер массива: ");
+        int.TryParse(Console.ReadLine(), out int n);
+        Console.Write("Введите определение массива: ");
+        int[] arrr = new int[n];
+        for (int i = 0; i < n; ++i)
+        {
+            arrr[i] = int.Parse(Console.ReadLine());
+        }
+        Console.Write("Array: ");
+        foreach (int i in arrr)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.Write("\nВведите счет: ");
+        int count = int.Parse(Console.ReadLine());
+        Console.Write("Введите показател: ");
+        int index = int.Parse(Console.ReadLine());
+
+
+        int[] newArr = new int[count];
+        int newArrSize = 0;
+        for (int i = index; i < Math.Min(count, n); ++i)
+        {
+            newArr[newArrSize++] = arrr[i];
+        }
+        if (newArrSize < count)
+        {
+            int baqiya = count - newArrSize;
+            for (int i = 0; i < baqiya; ++i)
             {
-                if (array[i] < array[i - 1]) min = i;
+                newArr[newArrSize++] = 1;
             }
-            Console.WriteLine("Минимальное значение массива: " + array[min]);
-            Console.ReadLine();
+        }
+        for (int i = 0; i < newArrSize; ++i)
+        {
+            Console.Write($"Это новый массив:{newArr[i]} ");
         }
     }
 
-// }
-
-
-         // Задание 3
-    //     class Program1
-    //     {
-    //         static void MyReverse(int[] array0)
-    //         {
-    //             int j = 0;
-    //             int[] array = new int[3];
-
-    //             for (int i = array0.Length-1; i >= 0; i--)
-    //             {
-    //                 array[j] = array0[i];
-    //                     j++;
-    //             }
-
-    //             for (int i=0;i< array0.Length; i++)
-    //             {
-    //                 array0[i] = array[i];
-    //             }
-
-        
-    //         }
-    // }     
-
-            // Задание 4
-    //  class Program2
-    //     {            
-    //         static void Main(string[] args, Console console)
-    //         {
-    //             int[] array0 = new int[3] { 9, 8, 7 };
-    //             MyReverse(array0);
-    //             for(int i = 0; i < array0.Length; i++)
-    //                 Console.Write("{0} ",array0[i]);
-    //             Console.ReadKey();
-                
-    //             Console.Write("Размерность массива: ");
-    //             int n = Convert.ToInt32(Console.ReadLine());  
-    //             int[] array = new int[n];
-    //             for (int i = 0; i < array.Length; i++)
-    //             {
-    //                 Console.Write("array[{0}] = ",i);
-    //                 array[i] = Convert.ToInt32(Console.ReadLine());
-    //             }
-    //             int min = 0;
-    //             for (int i = 1; i < array.Length; i++)
-    //             {
-    //                 if (array[i] < array[i - 1]) min = i; 
-    //             }
-    //             Console.WriteLine("Минимальное значение массива: " + array[min]);
-    //             Console.ReadLine();
-    //         }
-    //     }
 }
